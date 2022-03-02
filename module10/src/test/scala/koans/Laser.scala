e9ebe32e818e78e70f737ed082e2ddeb3ac9ea94
Nicolás Soria <nicolas.soria@hopin.to>
2022-03-02 00:01:51 -0300

    Adding module 10 code samples

--- module10/src/test/scala/koans/Laser.scala
+++ module10/src/test/scala/koans/Laser.scala
@@ -1,3 +1,14 @@
 
 /* Copyright (C) 2010-2018 Escalate Software, LLC. All rights reserved. */
 
+package lab.awesome.laser {
+  class Gun(val wattage: Int) {
+
+    class Beam(val lumens: Int)
+
+    def shoot(): Beam = {
+      val res = new Beam(wattage * 10)
+      res
+    }
+  }
+}
